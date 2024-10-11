20ffbdb9c643f58d9ccf9ed20da0217b0ea2c17a
dongming <501640144@qq.com>
2024-10-11 20:48:19 +0800

    调整接口适配2D粒子

--- src/layaAir/laya/display/cmd/DrawTrianglesCmd.ts
+++ src/layaAir/laya/display/cmd/DrawTrianglesCmd.ts
@@ -66,7 +66,9 @@ export class DrawTrianglesCmd {
      * @en Color transformation.
      * @zh 颜色变换。
      */
-    color: number | null;
+    color: number | number[] | null;
+
+    drawTriUseAbsMatrix: boolean = false;
 
     /**
      * @en Create a DrawTrianglesCmd instance
@@ -95,7 +97,7 @@ export class DrawTrianglesCmd {
      * @returns 绘制三角形命令实例
      */
     static create(texture: Texture, x: number, y: number, vertices: Float32Array, uvs: Float32Array, indices: Uint16Array,
-        matrix: Matrix | null, alpha: number, color: string | number, blendMode: string | null): DrawTrianglesCmd {
+        matrix: Matrix | null, alpha: number, color: string | number | number[], blendMode: string | null): DrawTrianglesCmd {
         var cmd: DrawTrianglesCmd = Pool.getItemByClass("DrawTrianglesCmd", DrawTrianglesCmd);
         cmd.texture = texture;
         texture._addReference();
@@ -106,7 +108,7 @@ export class DrawTrianglesCmd {
         cmd.indices = indices;
         cmd.matrix = matrix;
         cmd.alpha = alpha;
-        cmd.color = color != null ? ColorUtils.create(color).numColor : 0xffffffff;
+        cmd.color = color == null ? 0xffffff : typeof (color) == 'string' ? ColorUtils.create(color).numColor : color;
         cmd.blendMode = blendMode;
         return cmd;
     }
@@ -125,6 +127,7 @@ export class DrawTrianglesCmd {
         Pool.recover("DrawTrianglesCmd", this);
     }
 
+
     /**
      * @en Execute the drawing triangles command
      * @param context The rendering context
@@ -136,7 +139,11 @@ export class DrawTrianglesCmd {
      * @param gy 全局Y偏移  
      */
     run(context: Context, gx: number, gy: number): void {
-        context.drawTriangles(this.texture, this.x + gx, this.y + gy, this.vertices, this.uvs, this.indices, this.matrix, this.alpha, this.blendMode, this.color);
+        if (this.drawTriUseAbsMatrix && this.matrix) {
+            context.drawTrianglesAbs(this.texture, this.x + gx, this.y + gy, this.vertices, this.uvs, this.indices, this.matrix, this.alpha, this.blendMode, this.color);
+        } else {
+            context.drawTriangles(this.texture, this.x + gx, this.y + gy, this.vertices, this.uvs, this.indices, this.matrix, this.alpha, this.blendMode, this.color);
+        }
     }
 
     /**
