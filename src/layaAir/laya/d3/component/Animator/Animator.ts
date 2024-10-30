ef3233084d8cfe8ca343d4720fa929f55d397d2c
dongming <501640144@qq.com>
2024-10-30 14:37:42 +0800

    Animator特殊处理tOnce开头的trigger参数，设置trigger的一瞬间无法触发，下一瞬间就失效

--- src/layaAir/laya/d3/component/Animator/Animator.ts
+++ src/layaAir/laya/d3/component/Animator/Animator.ts
@@ -1444,6 +1444,19 @@ export class Animator extends Component {
                     break;
             }
         }
+
+        // 特殊处理 tOnce 开头的参数
+        for (const id in this._animatorParams) {
+            const name = AnimatorStateCondition.conditionIDToName(+id);
+            if (!name) {
+                console.error("Animator: unknown condition id", id);
+                continue;
+            }
+            const v = this._animatorParams[id];
+            if (name.startsWith("tOnce") && typeof v === "boolean") {
+                this._animatorParams[id] = false;
+            }
+        }
     }
 
     /**
