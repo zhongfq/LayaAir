3e67b6a3e14ae0946340c6bbb01d8a219af4c907
codetypes <codetypes@gmail.com>
2024-12-17 16:53:36 +0800

    fix fragment prefab reference

--- src/layaAir/laya/resource/Resource.ts
+++ src/layaAir/laya/resource/Resource.ts
@@ -58,6 +58,9 @@ class LinkedList<T extends Resource> {
             let node: ListNode<T> = Pool.createByClass<ListNode<T>>(ListNode);
             node.value = value;
             node.next = this._head;
+            if (this._tail == null) {
+                this._tail = node;
+            }
             this._head = node;
             this._length++;
         }
