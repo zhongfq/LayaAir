d7d8b81226b4cd43940f80a26867976bd5da1fee
codetypes <codetypes@gmail.com>
2024-12-20 11:25:04 +0800

    release texture holded by Value2D

--- src/layaAir/laya/renders/Context.ts
+++ src/layaAir/laya/renders/Context.ts
@@ -555,6 +555,14 @@ export class Context {
      */
     destroy(): void {
         --Context._contextcount;
+
+        for (let sv of this._shaderValueNeedRelease) {
+            sv.release();
+            sv._needRelease = false;
+        }
+        this._shaderValueNeedRelease.length = 0;
+        this.clear();
+
         this.sprite = null;
         this._charSubmitCache && this._charSubmitCache.destroy();
         if (this.defTexture) {
