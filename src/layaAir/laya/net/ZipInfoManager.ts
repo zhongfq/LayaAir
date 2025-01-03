561c0fecc3cc886b6a9b8cabeba9b7bef6be03ce
codetypes <codetypes@gmail.com>
2025-01-03 17:36:33 +0800

    add zip pack support

---
+++ src/layaAir/laya/net/ZipInfoManager.ts
@@ -0,0 +1,33 @@
+import { URL } from "./URL";
+
+class ZipEntry {
+    zipUrl: string;
+    path: string;
+}
+
+export class ZipInfoManager {
+    private static _fileDict: Record<string, ZipEntry> = {};
+    private static _zipDict: Record<string, string[]> = {};
+
+    static addZip(zipPath: string, files: string[]) {
+        const baseZipDir = zipPath.replace(/.zip$/, "/");
+        files.forEach((file) => {
+            const filePath = URL.formatURL(baseZipDir + file);
+            const zipUrl = URL.formatURL(zipPath);
+            ZipInfoManager._fileDict[filePath] = {zipUrl:zipUrl, path: file};
+        });
+        ZipInfoManager._zipDict[URL.formatURL(zipPath)] = files;
+    }
+
+    static getZipFiles(url: string): string[] | undefined {
+        return ZipInfoManager._zipDict[url];
+    }
+
+    static getEntryPath(url: string): string | undefined {
+        return ZipInfoManager._fileDict[url]?.path;
+    }
+
+    static getZipUrl(url: string): string | undefined {
+        return ZipInfoManager._fileDict[url]?.zipUrl;
+    }
+}
